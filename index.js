076ab913ca52ab47c985651ff6a81f08499cce7a
qwertin1998@gmail.com <herejutep1998>
2023-10-22 01:41:30 +0300

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser').json()
 const puppeteer = require('puppeteer');
 const app = express()
-const port = 3000
+const port = 3001
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const hook = new Discord.WebhookClient('1165062221777346731', 'XFjXcgk3qwTErMPinhdAUzhIaphi3nlNySb3XBjNL2sTlJHurf31eWs6flgpTd5gXZC3');
@@ -22,43 +22,51 @@ app.use(bodyParser)
 iteration = 0
 
 async function getEncounters() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://logs.stormforge.gg/guild/netherwing/hogwarts-legacy');
-    await page.setViewport({width: 1800, height: 1200})
-    let encounters = await page.evaluate(() => {
-        let encounters = Array.from(document.querySelectorAll('.list-group h6 a')).map(enc => {
-            return {
-                title: enc.innerText,
-                link: enc.href,
-            }
+
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://logs.stormforge.gg/guild/netherwing/hogwarts-legacy');
+        await page.setViewport({width: 1800, height: 1200})
+        let encounters = await page.evaluate(() => {
+            let encounters = Array.from(document.querySelectorAll('.list-group h6 a')).map(enc => {
+                return {
+                    title: enc.innerText,
+                    link: enc.href,
+                }
+            });
+            let dates = Array.from(document.querySelectorAll('.list-group span')).map((date,index) => {
+                return {
+                    title: encounters[index].title.replaceAll("'","''"),
+                    link: encounters[index].link,
+                    date: new Date(date.innerText).toLocaleDateString()
+                }
+            })
+            return dates
         });
-        let dates = Array.from(document.querySelectorAll('.list-group span')).map((date,index) => {
-            return {
-                title: encounters[index].title.replaceAll("'","''"),
-                link: encounters[index].link,
-                date: new Date(date.innerText).toLocaleDateString()
-            }
-        })
-        return dates
-    });
-
-
-    for await (const encounter of encounters){
-        const {rows} = await client.query(`SELECT * FROM encounters WHERE date = '${encounter.date}' and title = '${encounter.title}'`)
-        const isInDb = !!rows.length;
     
-        if (!isInDb){
-            console.log("Add new enctounter to db")
-            await client.query(`INSERT INTO encounters(title,date,link) VALUES('${encounter.title}','${encounter.date}','${encounter.link}')`);
-            const lootText = await getLoot(encounter.link)
-            hook.send(`\n [Гильдия убила](<${encounter.link}>) ${encounter.title}, ${lootText}`)
+    
+        for await (const encounter of encounters){
+            const {rows} = await client.query(`SELECT * FROM encounters WHERE date = '${encounter.date}' and title = '${encounter.title}'`)
+            const isInDb = !!rows.length;
+        
+            if (!isInDb){
+                console.log("Add new enctounter to db")
+                await client.query(`INSERT INTO encounters(title,date,link) VALUES('${encounter.title}','${encounter.date}','${encounter.link}')`);
+                const lootText = await getLoot(encounter.link)
+                const recountText = await getRecount(encounter.link)
+                hook.send(`\n [Гильдия убила](<${encounter.link}>) ${encounter.title} \n ${lootText} \n ${recountText}`);
+            }
         }
+        await browser.close();
+        console.log('Итерация', iteration)
+        iteration += 1
+    } catch (e){
+        console.log(e)
+    } finally {
+        getEncounters();
     }
-    await browser.close();
-    console.log('Итерация', iteration)
-    iteration += 1
-    getEncounters();
+    
 }
 
 const getLoot = async (link) => {
@@ -68,17 +76,41 @@ const getLoot = async (link) => {
     await page.waitForSelector(".raid-loot")
     let result = await page.evaluate(() => {
         let items = Array.from(document.querySelectorAll('.raid-loot .choices__item'));
-        console.log(items);
+        let images = Array.from(document.querySelectorAll('.raid-loot img')).map(img => img.src);
+        console.log(images);
         items = items.map(enc => {
             return {
                 title: enc.innerText,
                 link: enc.href,
             }
         })
-        return items.map(item => `[${item.title}](<${item.link}>)`)
+        return items.map((item,index) => `- [${item.title}](<${images[index]}>)`)
     });
     await browser.close()
-    return `\n **Лут**: ${result.join(', ')}`
+    return `\n **Лут**: \n  ${result.join('\n')}`
+}
+
+const getRecount = async (link) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(link);
+    await page.waitForSelector(".simple-log");
+
+    let result = await page.evaluate(() => {
+        let rows = Array.from(document.querySelectorAll('.simple-log .dmg-meter')).map(row => row.innerText.replaceAll('\n',' ').split(" "));
+        return rows.map((item,index) => `${index+1}. ${item[1]} *${item[2]}* **${item[3]}**`)
+    });
+
+    let totalRaidDps = 0
+    result.forEach(item => {
+        const regex = /\((\d+(?:[,.]\d+)?)\)/;
+        let match = regex.exec(item)[1];
+        match = match.replace(",","");
+        const toNumber = +match
+        if (!isNaN(toNumber)) totalRaidDps += toNumber
+    })
+    await browser.close()
+    return `\n**Дпс рейда** - **${totalRaidDps}**\n${result.join('\n')}`
 }
 
 const start = async () => {
